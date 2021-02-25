600aeb296f6a75f452b56f233f765600eb163f5f
Yoann Sanmartin <yoann.sanmartin@outlook.fr>
2021-02-25 09:22:34 +0100

    Added speed vector

--- multi_ocv.py
+++ multi_ocv.py
@@ -16,19 +16,33 @@ parser.add_argument('--manual', action = 'store_true')
 args = parser.parse_args()
 
 
-
 def drawBox(img, bbox):
     bbox = np.array(bbox).astype(int)
     x,y,w,h = bbox
     cv2.rectangle(img, (x,y), ((x+w), (y + h)), (255,255,255), 2, 1)
 
+
+def drawSpeed(img, prev_box, box):
+    box = np.array(box).astype(int)
+    x,y,w,h = box
+    xcentre = int(x + w/2)
+    ycentre = int(y + h/2)
+    prev_box = np.array(prev_box).astype(int)
+    x,y,w,h = prev_box
+    xcentreprev = int(x + w/2)
+    ycentreprev = int(y + h/2)
+    dx = xcentre - xcentreprev
+    dy = ycentre - ycentreprev
+    cv2.arrowedLine(img, (xcentre, ycentre), (xcentre+5*dx, ycentre+5*dy), (0, 255, 0), 2, 1)
+
+
 cam = cv2.VideoCapture("car-v3.mp4")
-trackers =  cv2.legacy.MultiTracker_create()
+trackers = cv2.legacy.MultiTracker_create()
 
 success, img = cam.read()
 if args.manual: 
     nb_detect = int(input('How many object to detect ? '))
-    for i in range(2): 
+    for i in range(nb_detect):
         box = cv2.selectROI("Car Tracking", img, fromCenter = False)
         # tracker = cv2.legacy.TrackerKCF_create()
         tracker = cv2.legacy.TrackerCSRT_create()
@@ -42,6 +56,8 @@ else:
         print('Box: {}'.format(box))
         trackers.add(tracker, img, box)
 
+prev_bbox = None
+
 while True: 
     timer = cv2.getTickCount()
     success, img = cam.read()
@@ -54,6 +70,12 @@ while True:
     else: 
         print('Lost')
         cv2.putText(img, 'Lost', (75,80), cv2.FONT_HERSHEY_COMPLEX, 0.7, (255,0,0), 2)
+
+    if prev_bbox is not None:
+        for (prev_box, box) in zip(prev_bbox, bbox):
+            drawSpeed(img, prev_box, box)
+
+    prev_bbox = bbox
 
     fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
     cv2.putText(img, str(int(fps)), (75,50), cv2.FONT_HERSHEY_COMPLEX, 0.7, (128,12,60), 2)
