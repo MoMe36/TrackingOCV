0e7ccbca088b2f3eb117ecbc4cec59c23825b94a
Moaad <moaad.taz@gmail.com>
2021-05-12 10:41:13 +0200

    initial commit

--- client.py
+++ client.py
@@ -2,22 +2,17 @@
 
 import socket
 
-def send(value):
+mysocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+mysocket.connect(('localhost',5566))
 
-	socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+def send_socket(client):
 
-try:
-
-    socket.connect(('localhost',5566))
-    print("connected client!")
-    data= value
+    data=input("write a msg: ")
     data=data.encode("utf8")
-    socket.sendall(data)
+    client.sendall(data)
+ 
 
 
 
-except:
-	print("communication to the server failed")
-
-finally:
-	#socket.close()
+while True:
+	send_socket(mysocket)
